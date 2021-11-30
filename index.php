f8901ff5d326705742ebe1fea405c9f949927836
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2021-11-30 13:09:51 +0100

    Create structure

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Mio CSS -->
+    <link rel="stylesheet" href="styles/main.css">
+
+    <title>Google - FAQ</title>
+</head>
+
+<body>
+
+    <header></header>
+
+    <main></main>
+
+    <footer></footer>
+
+</body>
+
+</html>
