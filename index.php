af6a837b157132ff874d93c14ae6a1471c487ebd
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2021-11-30 16:17:00 +0100

    add new data in maindata

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ $currentLinkNav = (count($listNavLink) - 1);
 $mainData = [
     [
         "title" => "<h2 class='m_bottom'>Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?</h2>",
-        "paragraph" => [
+        "paragraphs" => [
             "<p class='m_bottom_plus'>La recente <a href='#'>decisione della Corte di giustizia dell'Unione europea</a> ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.</p>",
             "<p class='m_bottom_plus'>Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.</p>",
             "<p class='m_bottom_plus'>Per presentare una richiesta di rimozione, compila questo <a href='#'>modulo web.</a> Riceverai una risposta automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di tempo perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio (eletto o non eletto). Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d'accordo con la nostra valutazione, puoi rivolgerti all'Autorità garante per la protezione dei dati personali nel tuo paese.</p>",
@@ -38,13 +38,58 @@ $mainData = [
     ],
     [
         "title" => "<h2 class='m_bottom'>Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?</h2>",
-        "paragraph" => [
+        "paragraphs" => [
             "<p class='m_bottom_plus'>Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.</p>",
             "<p class='m_bottom_plus'>Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.</p>",
             "<p class='m_bottom_plus'>Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel <a href='#'>Centro Google per la sicurezza online.</a></p>",
             "<p class='m_bottom_plus'><a href='#'>Scopri</a> quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.</p>",
         ],
     ],
+    [
+        "title" => "<h2 class='m_bottom'></h2>",
+        "paragraphs" => [
+            "<p class='m_bottom_plus'>Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:</p>",
+            "<ol class='m_bottom_plus'>
+                <li class='m_bottom_less'>
+                La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
+                    <ol>
+                        <li class='m_bottom_less'>Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</li>
+                        <li class='m_bottom_less'>Google LLC, con sede negli Stati Uniti, per il resto del mondo.</li>
+                    </ol>
+                </li>
+                <li class='m_bottom_less'>La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.</li>
+            </ol>",
+            "<p class='m_bottom_plus'>Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.</p>",
+        ],
+    ],
+    [
+        "title" => "<h2 class='m_bottom'></h2>",
+        "paragraphs" => [
+            "<p class='m_bottom_plus'></p>",
+            "<p class='m_bottom_plus'></p>",
+            "<p class='m_bottom_plus'></p>",
+            "<p class='m_bottom_plus'></p>",
+        ],
+    ],
+    [
+        "title" => "<h2 class='m_bottom'></h2>",
+        "paragraphs" => [
+            "<p class='m_bottom_plus'></p>",
+            "<p class='m_bottom_plus'></p>",
+            "<p class='m_bottom_plus'></p>",
+            "<p class='m_bottom_plus'></p>",
+        ],
+    ],
+    [
+        "title" => "<h2 class='m_bottom'></h2>",
+        "paragraphs" => [
+            "<p class='m_bottom_plus'></p>",
+            "<p class='m_bottom_plus'></p>",
+            "<p class='m_bottom_plus'></p>",
+            "<p class='m_bottom_plus'></p>",
+        ],
+    ],
+
 ]
 
 ?>
@@ -113,8 +158,10 @@ $mainData = [
             foreach ($mainData as $data) {
                 echo "<div class='help_section'>";
 
-                foreach ($data as $info) {
-                    echo $info["title"];
+                echo $data["title"];
+
+                foreach ($data["paragraphs"] as $text) {
+                    echo $text;
                 }
 
                 echo "</div>";
