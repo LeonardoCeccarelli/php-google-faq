6f4781549c09120fc603c0f6fcd2b5e2d008e1bd
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2021-11-30 13:11:24 +0100

    add fontawesome's cdn

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <!-- Link FONTAWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
     <!-- Mio CSS -->
     <link rel="stylesheet" href="styles/main.css">
 
@@ -14,7 +17,9 @@
 
 <body>
 
-    <header></header>
+    <header>
+        <nav></nav>
+    </header>
 
     <main></main>
 
